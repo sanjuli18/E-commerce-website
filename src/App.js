9a3693c76ada8c3dc3708d93c236bdbbf22de0e0
sanjuli18 <enlightenedsanjuli@gmail.com>
2021-08-02 12:02:35 +0530

    homepage has been updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import React from 'react';
 import Homepage from './pages/hompepage/homepage.component';
+import {Switch ,Route} from 'react-router-dom';
+
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE</h1>
+  </div>
+);
+ 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+      <Route exact path='/' component={Homepage}/>
+      <Route path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
